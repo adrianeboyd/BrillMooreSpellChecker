1c792191bbf6b795d0b6e76daec961bd39277225
Adriane Boyd <adriane@sfs.uni-tuebingen.de>
2014-10-06 11:22:57 +0200

    Removing EditOps

--- src/main/java/de/unituebingen/sfs/brillmoore/aligner/EditOps.java
+++
@@ -1,8 +0,0 @@
-package de.unituebingen.sfs.brillmoore.aligner;
-
-public class EditOps {
-	public final static String delete = "DEL";
-	public final static String insert = "INS";
-	public final static String match = "MAT";
-	public final static String sub = "SUB";
-}
