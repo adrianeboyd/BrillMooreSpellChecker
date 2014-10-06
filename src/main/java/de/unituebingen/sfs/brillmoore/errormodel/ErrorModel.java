ab6ed0a5cda4b37a1f221a4aa0ca0596ac8e0da5
Adriane Boyd <adriane@sfs.uni-tuebingen.de>
2014-10-06 09:56:29 +0200

    Import cleanup

--- src/main/java/de/unituebingen/sfs/brillmoore/errormodel/ErrorModel.java
+++ src/main/java/de/unituebingen/sfs/brillmoore/errormodel/ErrorModel.java
@@ -4,7 +4,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 import de.unituebingen.sfs.brillmoore.aligner.Alignment;
-import de.unituebingen.sfs.brillmoore.aligner.AlignmentUtils;
 
 public class ErrorModel {
 
